f71073b9f8027303d7c3389b4813aa635b55a220
Kevin Mcguigan <36888812+kkmcgg@users.noreply.github.com>
2023-07-21 15:45:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Three.js setup
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 2000; //added
+// camera.position.z = 2000; //added
 
 var renderer = new THREE.WebGLRenderer();
 
@@ -22,35 +22,35 @@ function onMouseMove(event) {
 }
 
 // Set up cubes for elements
-var geometry = new THREE.BoxGeometry(100, 100, 100);
+var geometry = new THREE.BoxGeometry(.5, .5, .5);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
 
-for (var i = 0; i < table.length; i += 5) {
-    var item = table[i];
+// for (var i = 0; i < table.length; i += 5) {
+//     var item = table[i];
 
-    var object = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color: colors[item]}));
-    object.position.x = (table[i + 3] * 140) - 1330; // position based on group
-    object.position.y = - (table[i + 4] * 180) + 900; // position based on period
-    object.position.z = 0;
+//     var object = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color: colors[item]}));
+//     object.position.x = (table[i + 3] * 140) - 1330; // position based on group
+//     object.position.y = - (table[i + 4] * 180) + 900; // position based on period
+//     object.position.z = 0;
 
-    // Set name to the object
-    object.name = table[i] + " (" + table[i + 1] + ")"; // Symbol (Element Name)
+//     // Set name to the object
+//     object.name = table[i] + " (" + table[i + 1] + ")"; // Symbol (Element Name)
 
-    scene.add(object);
-}
-
-// for(var i = 0; i < table.length; i += 5) {
-//     var element = new THREE.Mesh(geometry, material);
-//     element.position.x = table[i+3];
-//     element.position.y = table[i+4];
-//     element.userData = {
-//         symbol: table[i],
-//         name: table[i+1],
-//         atomic_weight: table[i+2]
-//     };
-//     scene.add(element);
+//     scene.add(object);
 // }
+
+for(var i = 0; i < table.length; i += 5) {
+    var element = new THREE.Mesh(geometry, material);
+    element.position.x = table[i+3];
+    element.position.y = table[i+4];
+    element.userData = {
+        symbol: table[i],
+        name: table[i+1],
+        atomic_weight: table[i+2]
+    };
+    scene.add(element);
+}
 
 camera.position.z = 5;
 
