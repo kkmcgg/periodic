736270315f5f800187a31f5c9f7eca8044bc0ade
Kevin Mcguigan <36888812+kkmcgg@users.noreply.github.com>
2023-07-21 15:47:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Three.js setup
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 20
 // camera.position.z = 2000; //added
 
 var renderer = new THREE.WebGLRenderer();
